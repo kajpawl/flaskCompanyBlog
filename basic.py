a0f33bd27bcb22b7e3bbc1ea1be6932ed0d2b82d
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-11-12 00:38:27 +0100

    Set up models and relations

--- basic.py
+++ basic.py
@@ -25,6 +25,12 @@ class Puppy(db.Model):
     name = db.Column(db.Text)
     age = db.Column(db.Integer)
     breed = db.Column(db.Text)
+    # ONE TO MANY
+    # Puppy to many Toys
+    toys = db.relationship('Toy', backref='puppy', lazy='dynamic')
+    # ONE TO ONE
+    # one Puppy to one Owner
+    owner = db.relationship('Owner', backref='puppy', uselist=False)
 
     def __init__(self, name, age, breed):
         self.name = name
@@ -32,4 +38,36 @@ class Puppy(db.Model):
         self.breed = breed
 
     def __repr__(self):
-        return f"Puppy {self.name} is a {self.age} years old {self.breed}."
+        if self.owner:
+            return f"Puppy name is {self.name} and owner is {self.owner.name}"
+        else:
+            return f"Puppy name is {self.name} and has no owner yet!"
+
+    def report_toys(self):
+        print("Here are my toys:")
+        for toy in self.toys:
+            print(toy.item_name)
+
+
+class Toy(db.Model):
+    __tablename__ = 'toys'
+
+    id = db.Column(db.Integer, primary_key=True)
+    item_name = db.Column(db.Text)
+    puppy_id = db.Column(db.Integer, db.ForeignKey('puppies.id'))
+
+    def __init__(self, item_name, puppy_id):
+        self.item_name = item_name
+        self.puppy_id = puppy_id
+
+
+class Owner(db.Model):
+    __tablename__ = 'owners'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    puppy_id = db.Column(db.Integer, db.ForeignKey('puppies.id'))
+
+    def __init__(self, name, puppy_id):
+        self.name = name
+        self.puppy_id = puppy_id

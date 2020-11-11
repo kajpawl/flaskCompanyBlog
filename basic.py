8a6398e98aee8642fc9d785ef98ab0cc2041fc6b
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-11-11 23:22:49 +0100

    Add flask_migrate

--- basic.py
+++ basic.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -11,6 +13,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+Migrate(app, db)
+
 
 ############################################
 class Puppy(db.Model):
@@ -20,10 +24,12 @@ class Puppy(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
     age = db.Column(db.Integer)
+    breed = db.Column(db.Text)
 
-    def __init__(self, name, age):
+    def __init__(self, name, age, breed):
         self.name = name
         self.age = age
+        self.breed = breed
 
     def __repr__(self):
-        return f"Puppy {self.name} is {self.age} years old."
+        return f"Puppy {self.name} is a {self.age} years old {self.breed}."

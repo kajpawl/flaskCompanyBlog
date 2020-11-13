dc8c5f363020e9b9b75c4dc7bf287b01ff5c684e
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-11-13 02:15:45 +0100

    Add views and forms, add owner model

--- adoption_site.py
+++ adoption_site.py
@@ -1,6 +1,6 @@
 import os
-# from forms import AddForm, DelForm
-from flask import Flask, render_template, url_for, redirect
+from forms import AddForm, DelForm, AddOwnerForm
+from flask import Flask, render_template, url_for, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -30,9 +30,97 @@ class Puppy(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
+    owner = db.relationship('Owner', backref='puppy', uselist=False)
 
     def __init__(self, name):
         self.name = name
 
     def __repr__(self):
-        return f"Puppy name: {self.name}."
+        if self.owner:
+            return f"Puppy name: {self.name}, owner: {self.owner.name}"
+        else:
+            return f"Puppy name: {self.name}, it has no owner yet!"
+
+
+class Owner(db.Model):
+    __tablename__ = 'owners'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    puppy_id = db.Column(db.Integer, db.ForeignKey('puppies.id'))
+
+    def __init__(self, name, puppy_id):
+        self.name = name
+        self.puppy_id = puppy_id
+
+    def __repr__(self):
+        return f"Owner name: {self.name}, puppy id: {self.puppy_id}."
+
+
+###########################################
+#####     VIEW FORMS - HAVE FORMS     #####
+###########################################
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_pup():
+    form = AddForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+
+        new_pup = Puppy(name)
+        db.session.add(new_pup)
+        db.session.commit()
+
+        return redirect(url_for('list_pup'))
+
+    return render_template('add.html', form=form)
+
+
+@app.route('/list')
+def list_pup():
+    puppies = Puppy.query.all()
+    return render_template('list.html', puppies=puppies)
+
+
+@app.route('/delete', methods=['GET', 'POST'])
+def del_pup():
+    form = DelForm()
+
+    if form.validate_on_submit():
+        id = form.id.data
+        pup = Puppy.query.get(id)
+
+        db.session.delete(pup)
+        db.session.commit()
+
+        return redirect(url_for('list_pup'))
+
+    return render_template('delete.html', form=form)
+
+
+@app.route('/add_owner', methods=['GET', 'POST'])
+def add_owner():
+    form = AddOwnerForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        puppy_id = form.puppy_id.data
+
+        new_owner = Owner(name, puppy_id)
+        db.session.add(new_owner)
+        db.session.commit()
+
+        flash(f'You just added the owner: {name}!')
+        return redirect(url_for('list_pup'))
+
+    return render_template('add_owner.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
